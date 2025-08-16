920bc7fe45854c7321ff4f62e86df88f7112422a
Prajualit Tickoo <23bec072@nith.ac.in>
2025-08-16 19:21:28 +0530

    components

---
+++ leetracker/src/components/ui/separator.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils"
+
+interface SeparatorProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation?: "horizontal" | "vertical"
+  decorative?: boolean
+}
+
+const Separator = ({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: SeparatorProps) => (
+  <div
+    className={cn(
+      "shrink-0 bg-border",
+      orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
+      className
+    )}
+    {...props}
+  />
+)
+
+export { Separator }
